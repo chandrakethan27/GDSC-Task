39270382034784adcb032a3690288b7329fad592
Chandrakethan <90309231+chandrakethan27@users.noreply.github.com>
2023-11-22 20:48:07 +0530

    Update eventSlice.jsx

--- src/slices/eventSlice.jsx
+++ src/slices/eventSlice.jsx
@@ -1,41 +1,5 @@
 import axios from 'axios';
 import {createAsyncThunk } from '@reduxjs/toolkit';
-
-// // eventSlice.js
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// // Define an initial state
-// const initialState = {
-//   events: [],
-// };
-
-// export const createEvent = createAsyncThunk('events/create', async (eventName) => {
-//   try {
-//     // Send a POST request to the backend to create the event
-//     const response = await axios.post('YOUR_BACKEND_URL/createEvent', { eventName });
-//     return response.data; // Assuming the backend responds with the created event data
-//   } catch (error) {
-//     throw error;
-//   }
-// });
-
-// // Define a reducer
-// const eventSlice = createSlice({
-//   name: 'events',
-//   initialState,
-//   reducers: {
-//   },
-//   extraReducers: (builder) => {
-//     builder.addCase(createEvent.fulfilled, (state, action) => {
-//       state.events.push(action.payload);
-//     });
-//   },
-// });
-
-// // Export actions and reducer
-// export const { /* Other actions */ } = eventSlice.actions;
-// export default eventSlice.reducer;
 import {createSlice} from '@reduxjs/toolkit'
 
 export const createEvent = createAsyncThunk('events/create', async (eventName) => {
